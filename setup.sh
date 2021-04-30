c3816fb55a13a572d815f408fe708e02a535388c
ItsNotAUsername <nik-off.fedotoff@yandex.ru>
2021-04-30 14:22:51 +0300

    Corrected setup(5).sh

--- setup.sh
+++ setup.sh
@@ -8,6 +8,6 @@ catkin_install_python(PROGRAMS scripts/control_node.py
 " >> CMakeLists.txt
 mkdir scripts
 cd scripts
-wget https://github.com/ITMO-SSR/Rice-On-Salad/blob/main/control_node.py -P catkin_ws/src/control_node/scripts
+wget https://raw.githubusercontent.com/ITMO-SSR/Rice-On-Salad/main/control_node.py
 cd ~/catkin_ws
 catkin_make
