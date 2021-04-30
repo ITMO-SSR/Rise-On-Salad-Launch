5008db90a9a33bc32b8d7a858381b9568362c104
ItsNotAUsername <nik-off.fedotoff@yandex.ru>
2021-04-30 13:47:00 +0300

    Corrected setup(2).sh

--- setup.sh
+++ setup.sh
@@ -1,4 +1,8 @@
-catkin_create_pkg ./catkin_ws/src/control_node rospy geometry_msgs.msg
-mkdir ./catkin_ws/src/control_node/scripts
+cd ~/catkin_ws/src
+catkin_create_pkg control_node rospy geometry_msgs.msg
+cd control_node
+mkdir scripts
+cd scripts
 wget https://github.com/ITMO-SSR/Rice-On-Salad/blob/main/control_node.py -P catkin_ws/src/control_node/scripts
+cd ~/catkin_ws
 catkin_make
