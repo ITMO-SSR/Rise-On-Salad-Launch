47f55203bd76156701c5a4b537bd41db0054ea5a
ItsNotAUsername <nik-off.fedotoff@yandex.ru>
2021-04-30 14:52:52 +0300

    final version(4)

--- launch.sh
+++ launch.sh
@@ -1,3 +1,3 @@
 chmod 777 catkin_ws/src/control_node/scripts/control_node.py
--e rosrun control_node control_node.py
--e rosrun teleop_twist_keyboard teleop_twist_keyboard.py
+rosrun control_node control_node.py &
+rosrun teleop_twist_keyboard teleop_twist_keyboard.py
