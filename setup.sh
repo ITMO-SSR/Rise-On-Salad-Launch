79280cf071eba984c44a1ddf373f1cfe3bbcdcb0
Nikita Fedotov <nik-off.fedotoff@yandex.ru>
2021-06-10 14:51:28 +0300

    Final version

--- setup.sh
+++ setup.sh
@@ -1,8 +1,9 @@
 cd ~/catkin_ws/src
-catkin_create_pkg control_node rospy geometry_msgs
-cd control_node
+git clone https://github.com/Slamtec/rplidar_ros.git
+catkin_create_pkg distance_node rospy sensor_msgs
+cd distance_node
 mkdir scripts
 cd scripts
-wget https://raw.githubusercontent.com/ITMO-SSR/Rice-On-Salad/main/control_node.py
+wget https://raw.githubusercontent.com/ITMO-SSR/Rice-On-Salad/main/programs/ros/distance_node.py
 cd ~/catkin_ws
 catkin_make
